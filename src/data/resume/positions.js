44bec68f0a3c45b7676712fc503f4303f627382e
danielnathaniel <danielyaghoobian@gmail.com>
2023-01-10 15:13:30 -0800

    fixed resume punctuation

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -6,8 +6,8 @@ const positions = [
     daterange: 'October 2022 - Present',
     points: [
       'Collaborated with multiple developers in a fast-paced, agile and cross-functional team',
-      'Assisted in designing UI.',
-      'Created training module for food runners.',
+      'Assisted in designing UI',
+      'Created training module for food runners',
       'Built and maintained reusable React components',
     ],
   },
@@ -18,8 +18,8 @@ const positions = [
     daterange: 'April 2015 - 2022',
     points: [
       'Managed all credit for the online lending platform',
-      'Underwrote 100MM of loan applications resulting in high quality debtors.',
-      'Maintained an average default rate of 5%, below the 8% average for the industry.',
+      'Underwrote 100MM of loan applications resulting in high quality debtors',
+      'Maintained an average default rate of 5%, below the 8% average for the industry',
     ],
   },
 ];
